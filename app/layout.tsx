87aee53d73ac87792d023f16bec577872740e164
David MANZI <manzidavid093@gmail.com>
2024-10-29 10:23:15 +0200

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import "./globals.css";
 import React from "react";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "Doxa",
+  description: "... Later ...",
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,11 +21,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        { children }
       </body>
     </html>
   )
